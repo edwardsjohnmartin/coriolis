e67beae2e76eff511eaf333105a92eaaba18d145
John Edwards <edwardsjohnmartin@gmail.com>
2019-11-04 10:13:00 -0700

    added label

--- diagram/main.js
+++ diagram/main.js
@@ -1,10 +1,18 @@
 function init() {
   let svg = document.getElementById('diagram');
 
-  var ellipse = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+  let ellipse = document.createElementNS("http://www.w3.org/2000/svg", 'path');
   ellipse.setAttribute("d","M 0 0 L 100 200"); //Set path's data
   ellipse.style.stroke = "#000"; //Set stroke colour
   ellipse.style.strokeWidth = "5px"; //Set stroke width
   svg.appendChild(ellipse);
+
+  let text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+  text.innerHTML = 'Hello world!';
+  text.setAttribute('x', 50);
+  text.setAttribute('y', 85);
+  text.setAttribute('fill', 'red');
+  svg.appendChild(text);
+
 }
 
