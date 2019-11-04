c0c36b6d26fb8ae0294422300da6480cd7c1367e
John Edwards <edwardsjohnmartin@gmail.com>
2019-11-04 10:17:01 -0700

    label changes

--- diagram/main.js
+++ diagram/main.js
@@ -1,13 +1,13 @@
 function init() {
   let svg = document.getElementById('diagram');
 
-  let ellipse = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-  ellipse.setAttribute("d","M 0 0 L 100 200"); //Set path's data
-  ellipse.style.stroke = "#000"; //Set stroke colour
-  ellipse.style.strokeWidth = "5px"; //Set stroke width
+  let ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  ellipse.setAttribute('d', 'M 0 0 L 100 200');
+  ellipse.style.stroke = 'black';
+  ellipse.style.strokeWidth = '5px';
   svg.appendChild(ellipse);
 
-  let text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+  let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
   text.innerHTML = 'Hello world!';
   text.setAttribute('x', 50);
   text.setAttribute('y', 85);
