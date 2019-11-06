602ecf49dfb527d7e97b40d83abd1a3c92b391cf
Cade Pankey <cade.pankey@aggiemail.usu.edu>
2019-11-06 13:29:03 -0700

    I've made an Ellipse as well as moved the Text around.

--- diagram/main.js
+++ diagram/main.js
@@ -2,15 +2,15 @@ function init() {
   let svg = document.getElementById('diagram');
 
   let ellipse = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-  ellipse.setAttribute('d', 'M 0 0 L 100 200');
+  ellipse.setAttribute('d', 'M 600 200 a 75,150 90 1,0 1,0');
   ellipse.style.stroke = 'black';
   ellipse.style.strokeWidth = '5px';
   svg.appendChild(ellipse);
 
   let text = document.createElementNS('http://www.w3.org/2000/svg', 'text');
-  text.innerHTML = 'Hello world!';
-  text.setAttribute('x', 50);
-  text.setAttribute('y', 85);
+  text.innerHTML = 'Cade';
+  text.setAttribute('x', 600);
+  text.setAttribute('y', 250);
   text.setAttribute('fill', 'red');
   svg.appendChild(text);
 
