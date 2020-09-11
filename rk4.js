0c78fa7a82c04eeb1dbe0d3fa5fa74320f719585
Uttam Khanal <kingofdelphi1992@gmail.com>
2020-09-11 08:55:04 +0545

    Add rk4 test

--- rk4.js
+++ rk4.js
@@ -7,3 +7,17 @@ const rk4 = (stepSize, position, derivative_funcs) => {
         return value + (delta1[idx] + 2 * delta2[idx] + 2 * delta3[idx] + delta4[idx]) / 6
     })
 }
+
+const rk4test = () => {
+    let x = 0
+    let v = 1
+    console.log({ x, v, sint: Math.sin(0), cost: Math.cos(0) })
+    let steps = 60
+    const stepSize = 0.02 * 2 * Math.PI
+    let t = 0
+    while (steps--) {
+        [x, v] = rk4(stepSize, [x, v], [(x, v) => v, (x, v) => -x])
+        t += stepSize
+        console.log({ x, v, sint: Math.sin(t), cost: Math.cos(t) })
+    }
+}
